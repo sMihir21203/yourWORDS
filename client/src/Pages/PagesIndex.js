bf8f369d410716f3bbed93a051d37bd8eea08bb5
smihir21203 <smihir997@gmail.com>
2025-02-05 13:51:13 +0530

    project setup and routes setup

---
+++ client/src/Pages/PagesIndex.js
@@ -0,0 +1,17 @@
+import About from "./About.jsx";
+import Dashboard from "./Dashboard.jsx";
+import Home from "./Home.jsx";
+import NotFound from "./NotFound.jsx";
+import Portfolio from "./Portfolio.jsx";
+import SignIn from "./SignIn.jsx";
+import SignUp from "./SignUp.jsx";
+
+export{
+    About,
+    Dashboard,
+    Home,
+    NotFound,
+    Portfolio,
+    SignIn,
+    SignUp
+}
---
+++ client/src/Routes.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  About,
+  Dashboard,
+  Home,
+  NotFound,
+  Portfolio,
+  SignIn,
+  SignUp,
+} from "./Pages/PagesIndex.js";
+import { Header } from "./Components/CompsIndex.js";
+
+export const WebRoutes = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element=<Home /> />
+          <Route path='/about' element=<About /> />
+          <Route path='/dashboard' element=<Dashboard /> />
+          <Route path='/portfolio' element=<Portfolio /> />
+          <Route path='/sign-in' element=<SignIn /> />
+          <Route path='/sign-up' element=<SignUp /> />
+          <Route path='/*' element=<NotFound /> />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
